09b7204397652255284f63ea78dca1ebd5b6d2ba
mikachu-chu <mikka.abarra@gmail.com>
2025-05-25 02:58:37 +0800

    v1.2

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { fly, slide, fade } from 'svelte/transition';
+  import { fly} from 'svelte/transition';
   import Page1 from './lib/page1.svelte';
   import Page2 from './lib/page2.svelte';
   import Page3 from './lib/page3.svelte';
@@ -17,15 +17,23 @@
 </script>
 
 <main
-  class="bg-linear-to-bl from-[#CCDCFF] to-[#805EBF]"
+  class="bg-linear-to-bl from-[#CCDCFF] to-[#805EBF] h-screen overflow-y-auto"
   in:fly={{ y: -500, duration: 1000 }}
 >
   <div class="flex flex-col w-full items-center">
-    <div class="mt-10 pb-5 w-xs sm:w-lg border-b place-items-center space-y-1">
-      <div class="w-full">
-        <img src="/headerLocked.png" alt="DEADLOCKED" />
+    <div class="mt-10 sm:mt-20 w-xs sm:w-lg border-b place-items-center">
+      <div class="flex justify-center items-end w-xs sm:w-lg">
+        <p class="text-4xl sm:text-6xl tracking-widest pb-3 sm:pb-5">DEADL</p>
+      <label class="swap swap-flip">
+         <input type="checkbox" />
+         
+         <div  class="swap-on"><img src="/Locked.png" alt="O" /></div>
+         <div class="swap-off"><img src="/Unlocked.png" alt="O" /></div>
+          
+      </label>
+      <p class="text-4xl sm:text-6xl tracking-widest pb-3 sm:pb-5">CKED</p>
       </div>
-      <h2 class="text-sm sm:text-lg text-center">
+      <h2 class="text-sm sm:text-lg text-center mt-1 mb-4 sm:mb-4">
         An Implementation of Banker's Algorithm
       </h2>
     </div>
@@ -56,10 +64,9 @@
       </div>
     {/if}
   </div>
-</main>
 
-<style>
-  /* * {
-    border: 1px solid black;
-  } */
-</style>
+  <div class="fixed bottom-0 shadow-xl bg-[#431259]/10 sm:bg-transparent w-screen justify-center text-xs sm:text-sm h-8 sm:h-10 items-center flex gap-2">
+    <a class="btn-circle btn h-6 w-6 bg-black text-white flex contents-center border-0" aria-label="button" type="button" href="https://github.com/mikachu-chu"><i class="ri-github-fill sm:scale-110"></i></a>
+      <span>Mikachu Chu</span>
+  </div>
+</main>
